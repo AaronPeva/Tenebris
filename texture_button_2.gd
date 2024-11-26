e438101d85f81bf93d714baa6b193d2c70417b4c
GodotColombia <jrocano@elpuig.xeill.net>
2024-11-26 15:34:52 +0100

    uuueap

--- texture_button_2.gd
+++ texture_button_2.gd
@@ -2,5 +2,7 @@ extends TextureButton
 
 
 # Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+var vidaEnemigo : TextureProgressBar
+
+func _pressed() -> void:
+	queue_free()
