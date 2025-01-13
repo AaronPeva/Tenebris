24b72ce12707e540af455b20cdc383105bbe0b14
GodotColombia <jrocano@elpuig.xeill.net>
2025-01-13 20:23:37 +0100

    Nodo corregido ahora si

--- node_2d.gd
+++ node_2d.gd
@@ -1,12 +1,36 @@
 extends Node2D
 
 
-# Called when the node enters the scene tree for the first time.
+
+@onready var cartas = $GCardHandLayout.get_children()
+@onready var vidaJugador = $vidaA
+@onready var vidaEnemigo = $vidaE
+@onready var enemyTimer = $Timer
+
 func _ready() -> void:
-	$BOTONPRUEBA.vidaEnemigo = $vidaE
-	pass # Replace with function body.
+	#$GCardHandLayout/TextureButton.vidaEnemigo = $vidaE
+	#$GCardHandLayout/TextureButton2.curar = $vidaE
+	#$GCardHandLayout/TextureButton3.vidaJugador = $vidaA
+	$GCardHandLayout/TextureButton.pressed.connect(
+		func(): _cuandoJugadorClicka())
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+var turnoJugador = true
+
+func _cuandoJugadorClicka() -> void:
+	if turnoJugador:
+		vidaEnemigo.value -= 1
+	#else:
+		turnoJugador = false
+		_esperarTurnoEnemigo()
+
+func _esperarTurnoEnemigo() -> void:
+	enemyTimer.start()
+	_turnoEnemigo()
+
+
+func _turnoEnemigo() -> void:
+	if not turnoJugador:
+		vidaJugador.value -= 1
+	#else:
+		turnoJugador = true
