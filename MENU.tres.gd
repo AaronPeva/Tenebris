64b13b191010d2d0b7013a2b160a691d0f1175d2
GodotColombia <jrocano@elpuig.xeill.net>
2025-02-14 16:25:41 +0100

    Mergeado con el compa

--- MENU.tres.gd
+++ MENU.tres.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 
 func _on_jugar_pressed() -> void:
-	get_tree().change_scene_to_file("res://cartafiumba.tscn")
+	get_tree().change_scene_to_file("res://main.tscn")
 
 
 func _on_opciones_pressed() -> void:
