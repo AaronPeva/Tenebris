9d769be8b87b4cecc77b20c3883c3122159db221
GodotColombia <jrocano@elpuig.xeill.net>
2025-02-14 15:17:11 +0100

    Menu v1

--- MENU.tres.gd
+++ MENU.tres.gd
@@ -2,15 +2,15 @@ extends Node2D
 
 
 func _on_jugar_pressed() -> void:
-	get_tree().change_scene_to_file("res://node_2d.tscn")
+	get_tree().change_scene_to_file("res://cartafiumba.tscn")
 
 
 func _on_opciones_pressed() -> void:
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://opciones.tscn")
 
 
 func _on_puntuaciones_pressed() -> void:
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://puntuaciones.tscn")
 
 
 func _on_salir_pressed() -> void:
