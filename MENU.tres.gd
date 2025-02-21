fec5d255a94e106353394f7afec7cd3589bdf10a
GodotColombia <jrocano@elpuig.xeill.net>
2025-02-21 15:11:41 +0100

    menu movimiento

--- MENU.tres.gd
+++ MENU.tres.gd
@@ -1,5 +1,18 @@
 extends Node2D
 
+@export var intensity: float = 3.0  # Ajusta la intensidad del efecto
+
+var original_position: Vector2
+
+func _ready():
+	original_position = position
+
+func _process(delta):
+	var mouse_pos = get_global_mouse_position()
+	var screen_center = get_viewport_rect().size / 2
+	var offset = (mouse_pos - screen_center) / screen_center * intensity
+	position = original_position + offset
+
 
 func _on_jugar_pressed() -> void:
 	get_tree().change_scene_to_file("res://main.tscn")
