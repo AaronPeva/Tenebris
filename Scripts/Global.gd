78c23e89abb9848672ad08ad68a19b669548f289
Peva <66553204+AaronPeva@users.noreply.github.com>
2025-04-22 16:20:53 +0200

    limpieza

--- Scripts/Global.gd
+++ Scripts/Global.gd
@@ -1,12 +1,4 @@
 extends Node
 
-#var carta_seleccionada = null  # Almacena la carta seleccionada
 var main = preload("res://Scenes/main.tscn")
-var escena_seleccionada = "res://Scenes/cardA1.tscn"  # Guardará null o la ruta de la escena
-#func seleccionar_carta(ruta_carta: String):
-	#carta_seleccionada = load(ruta_carta)  # Carga la escena de la carta
-#func _ready():
-	#if Global.carta_seleccionada != null:
-		#main.instantiate()
-		#add_child(carta_seleccionada)
-		#carta_seleccionada.position = Vector2(200, 300)
+var escena_seleccionada = "res://Scenes/cardA1.tscn"  
--- Scripts/card_manager.gd
+++ Scripts/card_manager.gd
@@ -9,7 +9,6 @@ var card_being_dragged
 var is_hovering_on_card
 var player_hand_reference
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	screen_size = get_viewport_rect().size
 	player_hand_reference = $"../PlayerHand"
@@ -32,13 +31,6 @@ func start_drag(card):
 
 func finish_drag():
 	card_being_dragged.scale = Vector2(0.3, 0.3)
-	#var card_slot_found = raycast_check_for_card_slot()
-	#if card_slot_found and not card_slot_found.card_in_slot:
-		#player_hand_reference.remove_card_from_hand(card_being_dragged)
-		#card_being_dragged.position = card_slot_found.position
-		#card_being_dragged.get_node("Area2D/CollisionShape2D").disabled = true
-		#card_slot_found.card_in_slot = true
-	#else:
 	player_hand_reference.add_card_to_hand(card_being_dragged, DEFAULT_CARD_MOVE_SPEED)
 	card_being_dragged = null
 
@@ -72,19 +64,6 @@ func highlight_card(card, hovered):
 	else:
 		card.scale = Vector2(0.3, 0.3)
 		card.z_index = 1
-
-#func raycast_check_for_card_slot():
-	#var space_state = get_world_2d().direct_space_state
-	#var parameters = PhysicsPointQueryParameters2D.new()
-	#parameters.position = get_global_mouse_position()
-	#parameters.collide_with_areas = true
-	#parameters.collision_mask = COLLISION_MASK_CARD_SLOT
-	#var result = space_state.intersect_point(parameters)
-	#if result.size() > 0:
-		#return result[0].collider.get_parent()
-	#return null
-
-
 
 func raycast_check_for_card():
 	var space_state = get_world_2d().direct_space_state
