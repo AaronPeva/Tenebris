b6add9faf21b8f12b61196f8333c778b311f5cb6
GodotColombia <jrocano@elpuig.xeill.net>
2025-04-25 17:03:25 +0200

    Posiciones preparadas para el bot

--- Scripts/main.gd
+++ Scripts/main.gd
@@ -14,7 +14,7 @@ func _ready():
 	salir.visible = false
 	if escena_cargada is PackedScene:  # Verificar que sea una escena válida
 		var instancia = escena_cargada.instantiate()  # Instanciar la escena
-		instancia.position = Vector2(600, 400) 
+		instancia.position = Vector2(576, 420) 
 		add_child(instancia) 
 		if instancia.has_signal("clic_personal"):
 			instancia.connect("clic_personal", _on_input_event)
@@ -28,6 +28,7 @@ func _process(delta):
 		indicadorturno.text = "Tu turno"
 	else:
 		indicadorturno.text = "Turno del rival"
+
 func _on_input_event():
 	_visible()
 
