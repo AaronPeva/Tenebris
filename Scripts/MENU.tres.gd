073dac975a4474129a767493456cfe960303110d
GodotColombia <jrocano@elpuig.xeill.net>
2025-05-06 15:35:44 +0200

    Multiplayer intento fallido

--- Scripts/MENU.tres.gd
+++ Scripts/MENU.tres.gd
@@ -15,7 +15,7 @@ func _process(delta):
 
 
 func _on_jugar_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/main.tscn")
+	get_tree().change_scene_to_file("res://Scenes/MENU.tscn")
 
 func _on_personajes_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/personajes.tscn")
