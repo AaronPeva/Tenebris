76e69f33c1e0e99d95b2ea13b6b93129fed8f2fe
Peva <66553204+AaronPeva@users.noreply.github.com>
2025-05-30 04:10:58 +0200

    Ajustes

--- Scripts/game/energy_node.gd
+++ Scripts/game/energy_node.gd
@@ -8,6 +8,8 @@ func _ready():
 	actualizar_color(3)
 
 func _process(delta):
+	if energ.text > str(5):
+		energ.text = "5"
 	if Global.puede_jugar and not ya_sumado:
 		var valor_actual = int(energ.text)
 		valor_actual = min(valor_actual + 1, 5)
@@ -15,6 +17,7 @@ func _process(delta):
 		energ.text = str(valor_actual)
 		ya_sumado = true
 		actualizar_color(valor_actual)
+	
 	elif not Global.puede_jugar:
 		ya_sumado = false 
 
